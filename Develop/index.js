7bd0ebd9ab3fddf382a0a7194fd040311dde0e94
jamesfill <jamesp.fillerup@gmail.com>
2022-03-24 10:09:15 -0600

    init function

--- Develop/index.js
+++ Develop/index.js
@@ -102,7 +102,14 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            writeToFile(answers)
+        })
+}
 
 // Function call to initialize app
 init();
